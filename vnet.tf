f022f50f7a7448d205c2cac5e96359fd49d60a7c
root <zhaohanqing@cloocus.com>
2020-11-27 07:49:52 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,6 +2,4 @@ module "network" {
   source  = "app.terraform.io/zhq-traning/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = "${var.prefix}-workshop" 
 }
-
